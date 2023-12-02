52980fdeed405383320afa0dee9e083ada339143
Davitt Barry <davittbarry333@gmail.com>
2023-12-02 08:51:18 +0200

    Adding character .png and basic movement

--- src/game.ts
+++ src/game.ts
@@ -1,66 +1,84 @@
 import * as Phaser from 'phaser';
 import './styles.css';
 
-const GRID_CELL_SIZE = 32;
-const GRID_WIDTH = Math.ceil(window.innerWidth / GRID_CELL_SIZE);
-const GRID_HEIGHT = Math.ceil(window.innerHeight / GRID_CELL_SIZE);
+// Cell definition
+const GRID_CELLS_WIDTH = 11;
+const GRID_CELLS_HEIGHT = 11;
+
+// Calculate the cell size based on the window size and the number of cells
+const GRID_CELL_SIZE = Math.min(
+  window.innerWidth / GRID_CELLS_WIDTH,
+  window.innerHeight / GRID_CELLS_HEIGHT,
+);
 
 class HelloWorldScene extends Phaser.Scene {
-  private player!: Phaser.GameObjects.Rectangle;
+  private player!: Phaser.GameObjects.Sprite;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
-  preload() {}
+  constructor() {
+    super('hello-world');
+  }
+
+  preload() {
+    this.load.image('mainchar', 'assets/mainchar1.png');
+  }
 
   create() {
-    const graphics = this.add.graphics({
-      lineStyle: { width: 1, color: 0xffffff },
-    });
+    //const graphics = this.add.graphics({
+    //  lineStyle: { width: 1, color: 0xffffff },
+    //});
 
-    // Draw vertical lines
-    for (let x = 0; x <= GRID_WIDTH * GRID_CELL_SIZE; x += GRID_CELL_SIZE) {
-      graphics.lineBetween(x, 0, x, GRID_HEIGHT * GRID_CELL_SIZE);
-    }
+    // Calculate the actual size of the grid
+    //const actualWidth = GRID_CELL_SIZE * GRID_CELLS_WIDTH;
+    //const actualHeight = GRID_CELL_SIZE * GRID_CELLS_HEIGHT;
 
-    // Draw horizontal lines
-    for (let y = 0; y <= GRID_HEIGHT * GRID_CELL_SIZE; y += GRID_CELL_SIZE) {
-      graphics.lineBetween(0, y, GRID_WIDTH * GRID_CELL_SIZE, y);
-    }
+    // Draw grid lines
+    //for (let x = 0; x <= actualWidth; x += GRID_CELL_SIZE) {
+    //  graphics.lineBetween(x, 0, x, actualHeight);
+    //}
+    //for (let y = 0; y <= actualHeight; y += GRID_CELL_SIZE) {
+    // graphics.lineBetween(0, y, actualWidth, y);
+    //}
 
-    // Create a simple square to represent the player
-    this.player = this.add
-      .rectangle(
-        GRID_CELL_SIZE / 2,
-        GRID_CELL_SIZE / 2,
-        GRID_CELL_SIZE,
-        GRID_CELL_SIZE,
-        0x0000ff,
-      )
-      .setOrigin(0, 0);
+    // Create the player sprite and set its size
+    this.player = this.add.sprite(
+      GRID_CELL_SIZE / 2,
+      GRID_CELL_SIZE / 2,
+      'mainchar',
+    );
+    this.player.setOrigin(0, 0);
+    // Calculate the scale for the player to fit within a grid cell
+    const playerScale = GRID_CELL_SIZE / this.player.width;
+    this.player.setScale(playerScale, playerScale);
 
+    // Setup event listeners for pointer clicks
     this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
-      // Calculate the grid cell coordinates
       const pointerX = Math.floor(pointer.x / GRID_CELL_SIZE) * GRID_CELL_SIZE;
       const pointerY = Math.floor(pointer.y / GRID_CELL_SIZE) * GRID_CELL_SIZE;
-
-      // Move the player to the clicked grid cell
-      this.player.setPosition(pointerX, pointerY);
+      this.tweens.add({
+        targets: this.player,
+        x: pointerX,
+        y: pointerY,
+        duration: 500,
+        ease: 'Linear',
+      });
     });
 
-    // Create cursor keys for movement
-    this.cursors = this.input.keyboard!.createCursorKeys();
+    // Create the cursor keys for movement
+    if (this.input && this.input.keyboard) {
+      this.cursors = this.input.keyboard.createCursorKeys();
+    }
   }
 
   update() {
-    if (!this.cursors || !this.player) {
-      return;
-    }
+    if (!this.cursors || !this.player) return;
 
+    // Player movement
     if (this.cursors.left.isDown) {
       this.player.x -= GRID_CELL_SIZE;
     } else if (this.cursors.right.isDown) {
       this.player.x += GRID_CELL_SIZE;
     }
-
     if (this.cursors.up.isDown) {
       this.player.y -= GRID_CELL_SIZE;
     } else if (this.cursors.down.isDown) {
@@ -71,14 +89,16 @@ class HelloWorldScene extends Phaser.Scene {
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: GRID_WIDTH * GRID_CELL_SIZE,
-  height: GRID_HEIGHT * GRID_CELL_SIZE,
+  width: GRID_CELL_SIZE * GRID_CELLS_WIDTH,
+  height: GRID_CELL_SIZE * GRID_CELLS_HEIGHT,
+  scene: [HelloWorldScene],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    parent: 'game-container',
   },
-  scene: HelloWorldScene,
+  parent: 'game-container',
 };
 
-new Phaser.Game(config);
+window.addEventListener('load', () => {
+  new Phaser.Game(config);
+});
